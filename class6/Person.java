ba4bf18f121a1534db2968752bb5d150b4bb7bb2
JeEon Park <ggungtoo@icloud.com>
2022-11-09 14:06:13 +0900

    java

---
+++ class6/Person.java
@@ -0,0 +1,12 @@
+package class6;
+
+import java.lang.*;
+
+public class Person {
+    public String gender;
+    public String name;
+    public int age;
+    public String birthday;
+
+    public String
+}
